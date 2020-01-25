29a25d89c22e2dd74a197619933539c1b3566a7a
Ganho Kou <publickohcz@gmail.com>
2020-01-25 17:45:13 +0900

    いったんコミット

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    WifiManager mWifiManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                boolean success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
+
+                if(success) {
+                    scanSuccess();
+                } else {
+                    scanFailure();
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        registerReceiver(wifiScanReceiver, intentFilter);
+
+        boolean success = mWifiManager.startScan();
+        if(!success) {
+            scanFailure();
+        }
+
+    }
+
+    private void scanSuccess() {
+        Log.d("スキャン成功","スキャン成功");
+        List<ScanResult> results = mWifiManager.getScanResults();
+        for(ScanResult result : results) {
+            Log.d(result.getClass().getSimpleName(), "結果/SSID:" + result.SSID);
+        }
+    }
+
+    private void  scanFailure() {
+        Log.d(mWifiManager.getClass().getSimpleName(), "失敗");
     }
 }
