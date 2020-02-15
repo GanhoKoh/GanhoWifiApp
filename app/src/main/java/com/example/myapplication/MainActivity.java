d7a958c96a756f6d96fcbe38c95aa546972a3c5d
Ganho Kou <publickohcz@gmail.com>
2020-02-15 14:21:19 +0900

    ここまででコミット

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,12 +10,30 @@ import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    WifiManager mWifiManager;
+    private WifiManager mWifiManager;
+    private ListView mListView;
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
+            if(success) {
+                   scanSuccess();
+            } else {
+                  scanFailure();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,40 +41,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+    }
 
-        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                boolean success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
-
-                if(success) {
-                    scanSuccess();
-                } else {
-                    scanFailure();
-                }
-            }
-        };
+    @Override
+    protected void onResume() {
+        super.onResume();
 
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        registerReceiver(wifiScanReceiver, intentFilter);
+        registerReceiver(mBroadcastReceiver, intentFilter);
 
         boolean success = mWifiManager.startScan();
-        if(!success) {
+        if(success) {
+            WifiManagerHolder wifiManagerHolder = WifiManagerHolder.getInstance();
+            wifiManagerHolder.setWifiManager(mWifiManager);
+            wifiManagerHolder.setBroadcastReceiver(mBroadcastReceiver);
+        } else {
             scanFailure();
         }
+    }
+
+    @Override
+    protected void onPause() {
+        unregisterReceiver(mBroadcastReceiver);
+        super.onPause();
 
     }
 
     private void scanSuccess() {
         Log.d("スキャン成功","スキャン成功");
         List<ScanResult> results = mWifiManager.getScanResults();
+
+        final ArrayList<String> wifiNameList = new ArrayList<>();
         for(ScanResult result : results) {
-            Log.d(result.getClass().getSimpleName(), "結果/SSID:" + result.SSID);
+            wifiNameList.add(result.SSID);
         }
+
+        ArrayAdapter<String> wifiList = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, wifiNameList);
+
+        mListView = findViewById(R.id.wifi_list);
+        mListView.setAdapter(wifiList);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, WifiInputActivity.class);
+                intent.putExtra("WifiSSID", wifiNameList.get(position));
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void  scanFailure() {
         Log.d(mWifiManager.getClass().getSimpleName(), "失敗");
     }
+
 }
--- app/src/main/java/com/example/myapplication/WifiInputActivity.java
+++ app/src/main/java/com/example/myapplication/WifiInputActivity.java
@@ -2,13 +2,34 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class WifiInputActivity extends AppCompatActivity {
+
+    Button mConnectBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_input);
+
+        Intent intent = getIntent();
+        String wifiSSID = intent.getStringExtra("WifiSSID");
+
+        TextView textView = findViewById(R.id.wifi_name_txt);
+        textView.setText(wifiSSID);
+
+        mConnectBtn = findViewById(R.id.connect_bttn);
+        mConnectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
 }
