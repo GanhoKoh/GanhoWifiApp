8077866beec2b43e221fa1619ecabd9c2a5f3816
Ganho Kou <publickohcz@gmail.com>
2020-03-28 11:26:56 +0900

    権限回りのパーミッションチェックを修正していったんコミット

--- app/src/main/java/com/example/myapplication/SplashActivity.java
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -1,18 +1,29 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        checkPermission();
+
+    }
+
+    private void moveToNext() {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -20,5 +31,34 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 2000L);
+    }
+
+    private void checkPermission() {
+        //6.0未満なのか？
+        if(Build.VERSION_CODES.M > Build.VERSION.SDK_INT) {
+            moveToNext();
+            return;
+        }
+
+        if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_REQUEST_CODE);
+        } else {
+            moveToNext();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(PERMISSION_REQUEST_CODE == requestCode) {
+            //権限がない場合のダイアログ結果振り分け
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //権限を許可したからそのままアプリ起動
+                moveToNext();
+            } else {
+                //許可がないと実行できないよの処理
+            }
+
+        }
     }
 }
